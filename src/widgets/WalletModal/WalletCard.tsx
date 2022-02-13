9e884c87c384d2b7e59600ee4e99a401d4c4ef07
mrjunying98 <82926228+mrjunying98@users.noreply.github.com>
2022-02-14 01:09:52 +0800

    changes

--- src/widgets/WalletModal/WalletCard.tsx
+++ src/widgets/WalletModal/WalletCard.tsx
@@ -16,13 +16,12 @@ const WalletCard: React.FC<Props> = ({ login, walletConfig, onDismiss, mb }) => 
   return (
     <Button
       fullWidth
-      variant="tertiary"
+      style={{ justifyContent: "space-between" , backgroundColor: "#000000", color: "#FFFFFF", lineHeight: "16px", fontSize: "0.88rem", boxShadow:"-0.16em -0.2em #FFFF00, 0.16em 0.2em #0000FF"}}
       onClick={() => {
         login(walletConfig.connectorId);
         window.localStorage.setItem(localStorageKey, "1");
         onDismiss();
       }}
-      style={{ justifyContent: "space-between" }}
       mb={mb}
       id={`wallet-connect-${title.toLocaleLowerCase()}`}
     >
--- src/widgets/WalletModal/config.tsx
+++ src/widgets/WalletModal/config.tsx
@@ -18,25 +18,11 @@ const connectors: Config[] = [
     connectorId: "injected",
   },
   {
-    title: "MathWallet",
-    icon: MathWallet,
-    connectorId: "injected",
-  },
-  {
-    title: "TokenPocket",
-    icon: TokenPocket,
-    connectorId: "injected",
-  },
-  {
     title: "WalletConnect",
     icon: WalletConnect,
     connectorId: "walletconnect",
   },
-  {
-    title: "Binance Chain Wallet",
-    icon: BinanceChain,
-    connectorId: "bsc",
-  },
+
 ];
 
 export default connectors;
